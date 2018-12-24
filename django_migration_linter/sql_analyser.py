f05b199925c08ec6bafbfd6d332c4f9b79a001b8
David-Wobrock <david.wobrock@gmail.com>
2018-12-24 16:05:38 +0100

    Black all files

--- django_migration_linter/sql_analyser.py
+++ django_migration_linter/sql_analyser.py
@@ -23,55 +23,58 @@ logger = logging.getLogger(__name__)
 
 
 def has_default(sql, **kwargs):
-    if re.search('SET DEFAULT', sql) and kwargs['errors']:
-        err = next(
-            (err for err in kwargs['errors'] if err['code'] == 'NOT_NULL'),
-            None,
-        )
+    if re.search("SET DEFAULT", sql) and kwargs["errors"]:
+        err = next((err for err in kwargs["errors"] if err["code"] == "NOT_NULL"), None)
         if err:
             logger.info(
-                ('Found a NOT_NULL error in migration, '
-                 'but it has a default value added: {}').format(err)
+                (
+                    "Found a NOT_NULL error in migration, "
+                    "but it has a default value added: {}"
+                ).format(err)
             )
-            kwargs['errors'].remove(err)
+            kwargs["errors"].remove(err)
 
     return False  # Never fails
 
 
 migration_tests = (
     {
-        'code': 'NOT_NULL',
-        'fn': lambda sql, **kw: re.search('NOT NULL', sql) and
-                not re.search('CREATE TABLE', sql),
-        'err_msg': 'NOT NULL constraint on columns'
-    }, {
-        'code': 'DROP_COLUMN',
-        'fn': lambda sql, **kw: re.search('DROP COLUMN', sql),
-        'err_msg': 'DROPPING columns'
-    }, {
-        'code': 'RENAME_COLUMN',
-        'fn': lambda sql, **kw: re.search('ALTER TABLE .* CHANGE', sql) or
-                re.search('ALTER TABLE .* RENAME COLUMN', sql),
-        'err_msg': 'RENAMING columns'
-    }, {
-        'code': 'RENAME_TABLE',
-        'fn': lambda sql, **kw: re.search('RENAME TABLE', sql) or
-                re.search('ALTER TABLE .* RENAME TO', sql),
-        'err_msg': 'RENAMING tables'
-    }, {
-        'code': 'ALTER_COLUMN',
-        'fn': lambda sql, **kw: re.search('ALTER TABLE .* MODIFY', sql),
-        'err_msg': ('ALTERING columns (Could be backward compatible. '
-                    'You may ignore this migration.)')
-    }, {
-        'code': '',
-        'fn': has_default,
-        'err_msg': ''
-    }, {
-        'code': IGNORED_MIGRATION,
-        'fn': lambda sql, **kw: re.search(IGNORE_MIGRATION_SQL, sql),
-        'err_msg': '',
-    }
+        "code": "NOT_NULL",
+        "fn": lambda sql, **kw: re.search("NOT NULL", sql)
+        and not re.search("CREATE TABLE", sql),
+        "err_msg": "NOT NULL constraint on columns",
+    },
+    {
+        "code": "DROP_COLUMN",
+        "fn": lambda sql, **kw: re.search("DROP COLUMN", sql),
+        "err_msg": "DROPPING columns",
+    },
+    {
+        "code": "RENAME_COLUMN",
+        "fn": lambda sql, **kw: re.search("ALTER TABLE .* CHANGE", sql)
+        or re.search("ALTER TABLE .* RENAME COLUMN", sql),
+        "err_msg": "RENAMING columns",
+    },
+    {
+        "code": "RENAME_TABLE",
+        "fn": lambda sql, **kw: re.search("RENAME TABLE", sql)
+        or re.search("ALTER TABLE .* RENAME TO", sql),
+        "err_msg": "RENAMING tables",
+    },
+    {
+        "code": "ALTER_COLUMN",
+        "fn": lambda sql, **kw: re.search("ALTER TABLE .* MODIFY", sql),
+        "err_msg": (
+            "ALTERING columns (Could be backward compatible. "
+            "You may ignore this migration.)"
+        ),
+    },
+    {"code": "", "fn": has_default, "err_msg": ""},
+    {
+        "code": IGNORED_MIGRATION,
+        "fn": lambda sql, **kw: re.search(IGNORE_MIGRATION_SQL, sql),
+        "err_msg": "",
+    },
 )
 
 
@@ -80,32 +83,23 @@ def analyse_sql_statements(sql_statements):
     ignored = False
     for statement in sql_statements:
         for test in migration_tests:
-            if test['fn'](statement, errors=errors):
-                if test['code'] == IGNORED_MIGRATION:
-                    logger.info(
-                        'Testing {0} -- IGNORING MIGRATION'.format(statement)
-                    )
+            if test["fn"](statement, errors=errors):
+                if test["code"] == IGNORED_MIGRATION:
+                    logger.info("Testing {0} -- IGNORING MIGRATION".format(statement))
                     ignored = True
                 else:
-                    logger.info('Testing {0} -- ERROR'.format(statement))
+                    logger.info("Testing {0} -- ERROR".format(statement))
                     table_search = re.search(
-                        'TABLE `([^`]*)`', statement, re.IGNORECASE)
-                    col_search = re.search(
-                        'COLUMN `([^`]*)`', statement, re.IGNORECASE)
+                        "TABLE `([^`]*)`", statement, re.IGNORECASE
+                    )
+                    col_search = re.search("COLUMN `([^`]*)`", statement, re.IGNORECASE)
                     err = {
-                        'err_msg':
-                            test['err_msg'],
-                        'code':
-                            test['code'],
-                        'table':
-                            table_search.group(1) if table_search else None,
-                        'column':
-                            col_search.group(1) if col_search else None
+                        "err_msg": test["err_msg"],
+                        "code": test["code"],
+                        "table": table_search.group(1) if table_search else None,
+                        "column": col_search.group(1) if col_search else None,
                     }
                     errors.append(err)
             else:
-                logger.info('Testing {0} -- PASSED'.format(statement))
-    return {
-        'errors': errors,
-        'ignored': ignored
-    }
+                logger.info("Testing {0} -- PASSED".format(statement))
+    return {"errors": errors, "ignored": ignored}
--- django_migration_linter/utils.py
+++ django_migration_linter/utils.py
@@ -21,12 +21,12 @@ from django_migration_linter.constants import MIGRATION_FOLDER_NAME
 
 
 def is_django_project(path):
-    django_manage_file = os.path.join(path, 'manage.py')
+    django_manage_file = os.path.join(path, "manage.py")
     return os.path.isfile(django_manage_file)
 
 
 def is_git_project(path):
-    git_directory = os.path.join(path, '.git')
+    git_directory = os.path.join(path, ".git")
     return os.path.isdir(git_directory)
 
 
@@ -44,33 +44,31 @@ def find_project_settings_module(path):
     """
     for root, dirs, files in os.walk(path):
         for file_name in files:
-            if file_name == 'settings.py':
-                return os.path.join(
-                    root.replace(path, ''),
-                    file_name).replace('/', '.').rstrip('.py')
+            if file_name == "settings.py":
+                return (
+                    os.path.join(root.replace(path, ""), file_name)
+                    .replace("/", ".")
+                    .rstrip(".py")
+                )
 
 
 def split_path(path):
     a, b = os.path.split(path)
-    return (split_path(a) if (len(a) > 0 and a != '/') else []) + [b]
+    return (split_path(a) if (len(a) > 0 and a != "/") else []) + [b]
 
 
 def split_migration_path(migration_path):
     decomposed_path = split_path(migration_path)
     for i, p in enumerate(decomposed_path):
         if p == MIGRATION_FOLDER_NAME:
-            return (decomposed_path[i-1],
-                    os.path.splitext(decomposed_path[i+1])[0])
+            return (decomposed_path[i - 1], os.path.splitext(decomposed_path[i + 1])[0])
 
 
 def compose_migration_path(django_folder, app_name, migration):
     return os.path.join(
-        django_folder,
-        app_name,
-        MIGRATION_FOLDER_NAME,
-        '{0}.py'.format(migration)
+        django_folder, app_name, MIGRATION_FOLDER_NAME, "{0}.py".format(migration)
     )
 
 
 def clean_bytes_to_str(byte_input):
-    return byte_input.decode('utf-8').strip()
+    return byte_input.decode("utf-8").strip()
