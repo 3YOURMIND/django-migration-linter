cc179c8bfb9de7945ee90366aaf4b12f8c73fc4d
David-Wobrock <david.wobrock@gmail.com>
2018-12-24 18:22:34 +0100

    Bump copyright version to 2019 in files

--- tests/functional/test_cmd_line_call.py
+++ tests/functional/test_cmd_line_call.py
@@ -1,4 +1,4 @@
-# Copyright 2018 3YOURMIND GmbH
+# Copyright 2019 3YOURMIND GmbH
 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
