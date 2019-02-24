c3c1af113c6da7087c39dd3358e63bd0dc59028a
David-Wobrock <david.wobrock@gmail.com>
2019-02-24 13:18:44 +0100

    Handle postgresql alter column type
    ALTER TABLE table ALTER COLUMN column TYPE

--- django_migration_linter/sql_analyser.py
+++ django_migration_linter/sql_analyser.py
@@ -63,7 +63,8 @@ migration_tests = (
     },
     {
         "code": "ALTER_COLUMN",
-        "fn": lambda sql, **kw: re.search("ALTER TABLE .* MODIFY", sql),
+        "fn": lambda sql, **kw: re.search("ALTER TABLE .* MODIFY", sql)
+        or re.search("ALTER TABLE .* ALTER COLUMN .* TYPE", sql),
         "err_msg": (
             "ALTERING columns (Could be backward compatible. "
             "You may ignore this migration.)"
