9df1bb930b59ccc424737527af679481c2f20b53
David Wobrock <david.wobrock@gmail.com>
2019-05-05 12:40:03 +0200

    Add support for Django 2.2 (by testing it in the CI)

--- setup.py
+++ setup.py
@@ -60,6 +60,7 @@ setup(
         "Framework :: Django :: 1.11",
         "Framework :: Django :: 2.0",
         "Framework :: Django :: 2.1",
+        "Framework :: Django :: 2.2",
         "Programming Language :: Python",
         "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 2.7",
