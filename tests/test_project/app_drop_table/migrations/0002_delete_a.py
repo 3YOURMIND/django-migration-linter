a4b49fe90eabbc9b9af18b39913557ed2691081d
David Wobrock <david.wobrock@gmail.com>
2019-07-29 21:14:29 +0200

    Black test app migration

--- tests/test_project/app_drop_table/migrations/0002_delete_a.py
+++ tests/test_project/app_drop_table/migrations/0002_delete_a.py
@@ -5,12 +5,6 @@ from django.db import migrations
 
 class Migration(migrations.Migration):
 
-    dependencies = [
-        ('app_drop_table', '0001_initial'),
-    ]
+    dependencies = [("app_drop_table", "0001_initial")]
 
-    operations = [
-        migrations.DeleteModel(
-            name='A',
-        ),
-    ]
+    operations = [migrations.DeleteModel(name="A")]
