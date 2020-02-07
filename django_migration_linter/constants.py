1925343f4aec22e00c582bc1700a8a43a3aa2d69
David Wobrock <david.wobrock@gmail.com>
2020-02-07 22:25:25 +0100

    Move constants to specific file and remove unused import

--- django_migration_linter/constants.py
+++ django_migration_linter/constants.py
@@ -3,3 +3,6 @@ from appdirs import user_cache_dir
 __version__ = "1.5.0"
 
 DEFAULT_CACHE_PATH = user_cache_dir("django-migration-linter", version=__version__)
+
+DJANGO_APPS_WITH_MIGRATIONS = ("admin", "auth", "contenttypes", "sessions")
+EXPECTED_DATA_MIGRATION_ARGS = ("apps", "schema_editor")
