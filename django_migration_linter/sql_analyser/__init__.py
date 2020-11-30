d207bf14b30636959e09659607bddcf4e349852b
David-Wobrock <david.wobrock@gmail.com>
2020-11-30 21:58:28 +0100

    Fix import order which was important

--- django_migration_linter/sql_analyser/__init__.py
+++ django_migration_linter/sql_analyser/__init__.py
@@ -1,5 +1,6 @@
-from .analyser import analyse_sql_statements  # noqa
 from .base import BaseAnalyser  # noqa
 from .mysql import MySqlAnalyser  # noqa
 from .postgresql import PostgresqlAnalyser  # noqa
 from .sqlite import SqliteAnalyser  # noqa
+
+from .analyser import analyse_sql_statements  # noqa isort:skip
