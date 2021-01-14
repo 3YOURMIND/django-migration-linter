12d188b5aed9fea4211098fb16c6e358a6fdb4b3
Andy Dau <tuananh.dau@robinhood.com>
2021-01-14 09:53:48 -0800

    Fix typo in makemigrations command

--- django_migration_linter/management/commands/makemigrations.py
+++ django_migration_linter/management/commands/makemigrations.py
@@ -14,7 +14,7 @@ from ..utils import register_linting_configuration_options
 
 def ask_should_keep_migration():
     questioner = InteractiveMigrationQuestioner()
-    msg = """\nThe migration linter detected that this migration is not be backward compatible.
+    msg = """\nThe migration linter detected that this migration is not backward compatible.
 - If you keep the migration, you will want to fix the issue or ignore the migration.
 - By default, the newly created migration file will be deleted.
 Do you want to keep the migration? [y/N]"""
