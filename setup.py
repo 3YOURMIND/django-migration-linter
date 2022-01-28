efd5d5989b61e0f4328a0f78a88ffbd6676bf723
David Wobrock <david.wobrock@gmail.com>
2022-01-28 20:52:04 +0100

    Add Django 4.0 support

--- setup.py
+++ setup.py
@@ -53,6 +53,7 @@ setup(
         "Framework :: Django",
         "Framework :: Django :: 2.2",
         "Framework :: Django :: 3.2",
+        "Framework :: Django :: 4.0",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.7",
--- tests/test_project/urls.py
+++ tests/test_project/urls.py
@@ -13,7 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls import url
 from django.contrib import admin
+from django.urls import path
 
-urlpatterns = [url("admin/", admin.site.urls)]
+urlpatterns = [path("admin/", admin.site.urls)]
